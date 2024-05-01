cb9a350f034aa8b3a9509d4ea0246a288d05ef4f
rouVling <youqianiqian@126.com>
2024-05-01 16:29:50 +0800

    live2d init

--- electron.vite.config.ts
+++ electron.vite.config.ts
@@ -12,7 +12,8 @@ export default defineConfig({
   renderer: {
     resolve: {
       alias: {
-        '@renderer': resolve('src/renderer/src')
+        '@renderer': resolve('src/renderer/src'),
+        '@framework': resolve('src/renderer/Framework/src'),
       }
     },
     plugins: [react()]
