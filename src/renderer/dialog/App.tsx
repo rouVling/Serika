1461608fea4184f4252f05e7b84c51f33324a857
rouVling <youqianiqian@126.com>
2024-05-03 02:25:14 +0800

    added input

--- src/renderer/dialog/App.tsx
+++ src/renderer/dialog/App.tsx
@@ -1,11 +1,24 @@
 import React from "react"
 
+import { TextField } from "@mui/material"
+
 export default function App(): JSX.Element {
 
   const [input, setInput] = React.useState<string>("")
 
+  const handleEnter = (e: React.KeyboardEvent<HTMLInputElement>) => {
+    if(e.key==="Enter"){setInput("")}
+  }
+
   return (
-    <input id="UserInput" type="text" value={input} onChange={(e) => setInput(e.target.value)}></input>
-    // <TextField id="UserInput" label="input" variant={"outlined"} sx={{color:"primary", height:"50%"}}/>
+    <input
+    id="UserInput"
+    type="text"
+    value={input}
+    onChange={(e)=>{setInput(e.target.value);}}
+    onKeyDown={handleEnter}
+    ></input>
+    // <TextField id="UserInput" label="input" variant={"standard"} sx={{color:"primary"}}/>
   )
 }
+
